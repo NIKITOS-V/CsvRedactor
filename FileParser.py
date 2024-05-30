f23afc5acead916d25372d3b96c78d38494c2e94
Nik <vayrikh.n@gmail.com>
2024-05-30 17:41:58 +0700

    First Commit

---
+++ FileParser.py
@@ -0,0 +1,15 @@
+class FileParser:
+    TB = '\t'
+    NL = '\n'
+
+    def __init__(self):
+        self.CommandsDict = {}
+
+    def MainLoop(self):
+        pass
+
+    def ConsoleError(self, Message):
+        print(f"{self.NL}{self.TB}CONSOLE ERROR: {Message}")
+
+    def CheckingForFileAvailability(self):
+        pass
